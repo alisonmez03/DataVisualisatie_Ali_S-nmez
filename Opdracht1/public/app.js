cac6c090a1bd84c1589d6dd698bdf0c51031d6ce
alisonmez03 <ali.sonmmez@student.ehb.be>
2018-11-09 15:33:08 +0100

    data opgehaald van csv file

---
+++ Opdracht1/public/app.js
@@ -0,0 +1,22 @@
+var nasa = {};
+
+var datarequest = new XMLHttpRequest();
+datarequest.open('GET', '/data', true);
+datarequest.onload = function () {
+    //load data
+    var data = JSON.parse(this.response);
+    console.log(data);
+    
+    let str = "<ul>";
+    data.forEach(element => {
+        str += "<li>" + element.name + " -- " + element.mass + "</li>";
+    });
+    str += "</ul>";
+    //populate raw data
+    document.getElementById("data").innerHTML = str;
+    //assign globals
+    nasa.data = data.nasa;
+}
+
+// Send request
+datarequest.send();
